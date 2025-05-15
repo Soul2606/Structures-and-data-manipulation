45347663a78ea40b2e85671accf93bacbb24ddda
Soul2606 <sondulv@gmail.com>
2025-05-15 10:42:57 +0200

    added stuff and things

--- script.js
+++ script.js
@@ -8,6 +8,16 @@ a replaceMe function that can be used to mutate the original json structure used
 
 
 const dataStructureContainer = document.getElementById('data-structure-container')
+const addButtonContainer = document.getElementById('add-button-container')
+const addString = document.getElementById('add-string')
+const addNumber = document.getElementById('add-number')
+const addBool = document.getElementById('add-bool')
+const addDictionary = document.getElementById('add-dictionary')
+const addArray = document.getElementById('add-array')
+const addNull = document.getElementById('add-null')
+const setStringButton = document.getElementById('set-string-button')
+const setStringContainer = document.getElementById('set-string-container')
+const setString = document.getElementById('set-string')
 
 
 
@@ -57,7 +67,7 @@ function createHTMLStructure(json, elementCreationFunction=()=>{}) {
 
 			const keyElement = document.createElement('p')
 			keyElement.className = 'object-key'
-			keyElement.textContent = key
+			keyElement.innerHTML = key + '<span style="color: white; margin-left: 5px;">:</span>'
 			root.appendChild(keyElement)
 
 			const elementContainer = document.createElement('div')
@@ -77,32 +87,33 @@ function createHTMLStructure(json, elementCreationFunction=()=>{}) {
 		root.className = 'array'
 
 		const topBracket = document.createElement('div')
+		topBracket.className = 'array-bracket'
 		topBracket.style.height = '8px'
-		topBracket.style.border = 'solid magenta 2px'
 		topBracket.style.borderRadius = '3px 3px 0 0'
 		topBracket.style.borderBottom = '0'
 		root.appendChild(topBracket)
 
+		const arrayGrid = document.createElement('div')
+		arrayGrid.className = 'array-grid'
+		root.appendChild(arrayGrid)
+
 		for (let i = 0; i < elements.length; i++) {
 			const element = elements[i]
-			const item = document.createElement('div')
-			item.className = 'index-value'
-			root.appendChild(item)
 
 			const indexElement = document.createElement('p')
 			indexElement.className = 'array-index'
-			indexElement.textContent = i + ':'
-			item.appendChild(indexElement)
+			indexElement.innerHTML = i + '<span style="color: white; margin-left: 5px;">:</span>'
+			arrayGrid.appendChild(indexElement)
 
 			const elementContainer = document.createElement('div')
 			elementContainer.className = 'array-value'
 			elementContainer.appendChild(element)
-			item.appendChild(elementContainer)
+			arrayGrid.appendChild(elementContainer)
 		}
 
 		const bottomBracket = document.createElement('div')
+		bottomBracket.className = 'array-bracket'
 		bottomBracket.style.height = '8px'
-		bottomBracket.style.border = 'solid magenta 2px'
 		bottomBracket.style.borderRadius = '0 0 3px 3px'
 		bottomBracket.style.borderTop = '0'
 		root.appendChild(bottomBracket)
@@ -113,14 +124,31 @@ function createHTMLStructure(json, elementCreationFunction=()=>{}) {
 	const createValueElement = (value)=>{
 		const root = document.createElement('p')
 		root.className = 'value'
-		root.textContent = String(value)
+		switch (typeof value) {
+		case 'string':
+			root.classList.add('string')
+		break;
+		case 'number':
+			root.classList.add('number')
+		break;
+		case 'bool':
+			root.classList.add('bool')
+		break;
+		}
+		if (typeof value === 'string') {
+			root.textContent = `"${value}"`
+		}else if (value instanceof HTMLElement) {
+			root.appendChild(value)
+		}else{
+			root.textContent = String(value)
+		}
 		return root
 	}
 
-	const recursiveFunction = (object, visited = new Set(), replaceMe=(newValue)=>{json = newValue})=>{
+	const recursiveFunction = (object, visited = new Set(), replaceMe=(newValue)=>{json = newValue}, parent=null)=>{
 		if (typeof object !== 'object' || object === null) {
 			const valueElement = createValueElement(object)
-			elementCreationFunction(valueElement, object, replaceMe)
+			elementCreationFunction(valueElement, object, replaceMe, parent)
 			return valueElement
 		}
 		visited.add(object)
@@ -128,12 +156,12 @@ function createHTMLStructure(json, elementCreationFunction=()=>{}) {
 			const arrayElement = createArrayElement(object.map((element, index)=>{
 				if (visited.has(element)) {
 					const valueElement = createValueElement('visited')
-					elementCreationFunction(valueElement, element, replaceMe)
+					elementCreationFunction(valueElement, element, replaceMe, parent)
 					return valueElement
 				}
-				return recursiveFunction(element, visited, (newValue)=>{object[index] = newValue})
+				return recursiveFunction(element, visited, (newValue)=>{object[index] = newValue}, {data:object, replaceMe, parent})
 			}))
-			elementCreationFunction(arrayElement, object, replaceMe)
+			elementCreationFunction(arrayElement, object, replaceMe, parent)
 			return arrayElement
 		}else{
 			const elements = []
@@ -146,14 +174,14 @@ function createHTMLStructure(json, elementCreationFunction=()=>{}) {
 				keys.push(key)
 				if (visited.has(element)) {
 					const valueElement = createValueElement('visited')
-					elementCreationFunction(valueElement, element, replaceMe)
+					elementCreationFunction(valueElement, element, replaceMe, parent)
 					elements.push(valueElement)
 				}else{
-					elements.push(recursiveFunction(element, visited, (newValue)=>{object[key] = newValue}))
+					elements.push(recursiveFunction(element, visited, (newValue)=>{object[key] = newValue}, {data:object, replaceMe, parent}))
 				}
 			}
 			const objectElement = createObjectElement(elements, keys)
-			elementCreationFunction(objectElement, object, replaceMe)
+			elementCreationFunction(objectElement, object, replaceMe, parent)
 			return objectElement
 		}
 	}
@@ -164,21 +192,88 @@ function createHTMLStructure(json, elementCreationFunction=()=>{}) {
 
 
 
-fetchJSON('structure.json').then(response=>{
-	console.log(response)
-	dataStructureContainer.appendChild(createHTMLStructure(response, (valueElement, data, replaceMe)=>{
-		if (!(valueElement instanceof HTMLElement)) {
-			throw new Error("valueElement is not an instance of HTMLElement");
+function removeAllChildren(element) {
+	if (!(element instanceof HTMLElement)) {
+		console.error(element)
+		throw new Error("element is not instance of HTMLElement");
+	}
+	while (element.hasChildNodes()) {
+		element.firstChild.remove()
+	}
+}
+
+
+
+
+function deserializeJSON(json) {
+	addButtonContainer.style.display = 'none'
+	setStringContainer.style.display = 'none'
+	removeAllChildren(dataStructureContainer)
+	dataStructureContainer.appendChild(createHTMLStructure(json, elementActions))
+	
+	const selected = {element:null, data:null, replaceMe:null, parent:null}
+	function elementActions(element, data, replaceMe, parent) {
+		if (!(element instanceof HTMLElement)) {
+			console.error(element)
+			throw new Error("element is not an instance of HTMLElement");
 		}
 		if (typeof replaceMe !== 'function') {
 			console.error(replaceMe)
 			throw new Error("replaceMe is not a function");
 		}
-		valueElement.addEventListener('click',e=>{
+		element.addEventListener('click',e=>{
 			e.stopPropagation()
-			console.log('click',valueElement, data, replaceMe)
-			replaceMe('kill')
-			console.log(response)
+			selected.element? selected.element.classList.remove('selected'): null;
+			element.classList.add('selected')
+			selected.element = element
+			selected.data = data
+			selected.replaceMe = replaceMe
+			selected.parent = parent
+
+			if (typeof data === 'object') {
+				addButtonContainer.style.display = 'block'
+			}else{
+				addButtonContainer.style.display = 'none'
+			}
+
+			if (typeof data === 'string') {
+				setStringContainer.style.display = 'block'
+			}else{
+				setStringContainer.style.display = 'none'
+			}
 		})
-	}))
+	}
+	return selected
+}
+
+
+
+
+fetchJSON('structure.json').then(response=>{
+	console.log(response)
+	let jsonDatastructure = response
+	
+	let selected = deserializeJSON(jsonDatastructure)
+	
+	addString.addEventListener('click',()=>{
+		const data = selected.data
+		if (Array.isArray(data)) {
+			data.push('')
+		}else if (typeof data === 'object') {
+			data['key'] = ''
+		}else{
+			console.error(data)
+			throw new Error("data is not an object");
+		}
+		selected = deserializeJSON(jsonDatastructure)
+	})
+	
+	setStringButton.addEventListener('click',()=>{
+		selected.replaceMe(setString.value)
+		selected = deserializeJSON(jsonDatastructure)
+	})
 })
+
+
+
+
